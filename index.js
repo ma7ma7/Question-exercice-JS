7415a96b36c21917a75149264e1b3a0dd8281c86
mahmoud <mahmoudamane5@gmail.com>
2020-05-15 21:42:18 +0000

    My solution one with function of class

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 /*
 --- Let's build a fun quiz game in the console! ---
 
@@ -19,3 +18,85 @@ c) correct answer (I would use a number for this)
 
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
+
+var Question = function(question, responses, correctAnswer) {
+    this.question = question;
+    this.responses = responses;
+    this.correctAnswer = correctAnswer
+}
+
+Question.prototype.getQuestion = function(questions) {
+    var quest = questions[Math.floor(Math.random() * Math.floor(3))]
+    console.log(quest.question + ' ?');
+    for(var i = 0; i <  quest.responses.length; i++) {
+        console.log(quest.responses[i].id + ' : ' + quest.responses[i].response);
+    }
+
+    var prompt = window.prompt("Select the correct Answer ? (Enter a number)");
+    console.log(prompt)
+
+    if(prompt == quest.correctAnswer) {
+        console.log('You win')
+    }
+}
+
+var questOne = new Question(
+    'What is hoisting',
+    [
+        {
+            id: 0,
+            response: 'Is a toy'
+        },
+        {
+            id: 1,
+            response: 'is the way of calling functions and variables'
+        },
+        {
+            id: 2,
+            response: 'is an unsusfull feature'
+        }
+    ],
+    1
+);
+
+
+var questTwo = new Question(
+    'Is JS Fun',
+    [
+        {
+            id: 0,
+            response: 'Yes'
+        },
+        {
+            id: 1,
+            response: 'No'
+        }
+    ],
+    0
+);
+
+
+var questThree = new Question(
+    'What is my name',
+    [
+        {
+            id: 0,
+            response: 'Jalal'
+        },
+        {
+            id: 1,
+            response: 'mahmoud'
+        },
+        {
+            id: 2,
+            response: 'ussef'
+        }
+    ],
+    1
+);
+
+var arrayQuest = [questOne, questTwo, questThree];
+
+
+
+Question.prototype.getQuestion(arrayQuest);
