0b232c5f4d09f38fc407a5d43801828c69cf562e
mahmoud <mahmoudamane5@gmail.com>
2020-05-16 00:51:05 +0000

    The last modification

--- index.js
+++ index.js
@@ -17,6 +17,16 @@ c) correct answer (I would use a number for this)
 6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
 
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+
+--- Expert level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+11. Display the score in the console. Use yet another method for this.
 */
 
 (function(){
@@ -34,10 +44,24 @@ c) correct answer (I would use a number for this)
         }
     }
 
-    Question.prototype.checkAnswer = function (enterd) {
+    Question.prototype.checkAnswer = function (enterd, callback) {
+
+        var sr = 0;
+
         if (enterd == this.correctAnswer) {
             console.log('You are correct')
+            sr = callback(true)
+        } else {
+            console.log('Error answer');
+            sr = callback(false)
         }
+
+        this.displayScore(sr)
+    }
+
+    Question.prototype.displayScore = function (sr) {
+        console.log('Your score is : ' + sr);
+        console.log('---------------------------------')
     }
 
     var questOne = new Question(
@@ -95,14 +119,35 @@ c) correct answer (I would use a number for this)
         1
     );
 
+    function score () {
+        var sr = 0;
+        return function(correct) {
+            if (correct) {
+                sr++;
+            }
+        }
+    }
+
+    var setScore = score();
+
     var arrayQuest = [questOne, questTwo, questThree];
-    var n = Math.floor(Math.random() * arrayQuest.length);
 
-    arrayQuest[n].getQuestion();
-
-    var answer = parseInt(
+    function nextQuest() {
+        var n = Math.floor(Math.random() * arrayQuest.length);
+        
+        arrayQuest[n].getQuestion();
+        
+        var answer = parseInt(
             prompt("Select the correct Answer ? (Enter a number)")
         );
-    arrayQuest[n].checkAnswer(answer);
+
+        if (answer !== 'exit') {
+            arrayQuest[n].checkAnswer(answer, setScore);
+            nextQuest();
+        }
+
+    }
+
+    nextQuest();
 
 })();
